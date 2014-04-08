4b470d3f0224f90599b454c96e3d440a383f808e
Paweł Szymański <out.of.logins.error@gmail.com>
2014-04-08 16:03:46 +0200

    osgControl WIP: osgCanvas No wxGraphicsWindow. No change in mainwindow.h.

--- osgcontrol.cpp
+++ osgcontrol.cpp
@@ -1,27 +1,107 @@
 #include "osgcontrol.hpp"
 
-osgControl::osgControl()
+osgCanvas::osgCanvas(wxWindow *parent, wxWindowID id, const wxPoint& pos, const wxSize& size, long style, const wxString& name, int *attributes) : wxGLCanvas(parent, id, pos, size, style|wxFULL_REPAINT_ON_RESIZE, name, attributes)
 {
-  Viewer=new osgViewer::Viewer();
-
-  WXWidget handle =GetHandle();
-
-  osg::ref_ptr<osg::GraphicsContext::Traits> traits = new osg::GraphicsContext::Traits();
-  traits->inheritedWindowData = new osgViewer::GraphicsWindowX11::WindowData(handle);
-
-osg::ref_ptr<osg::GraphicsContext> gc = osg::GraphicsContext::createGraphicsContext( traits.get() );
-viewer->getCamera()->setGraphicsContext( gc.get() );
-viewer->getCamera()->setViewport( new osg::Viewport( 0, 0, traits->width, traits->height ) );
-viewer->getCamera()->setProjectionMatrixAsPerspective(30.0f, static_cast<double>(traits->width)/static_cast<double>(traits->height), 1.0f, 10000.0f);
 }
 
-
-osgControl::~osgControl()
+osgCanvas::~osgCanvas()
 {
-  delete Viewer;
 }
 
-void osgControl::OnPaint(wxPaintEvent& paintEvent)
+void osgCanvas::OnPaint( wxPaintEvent& WXUNUSED(event) )
 {
-
+  /* must always be here */
+  wxPaintDC dc(this);
 }
+
+void osgCanvas::OnSize(wxSizeEvent& event)
+{
+  // this is also necessary to update the context on some platforms
+  wxGLCanvas::OnSize(event);
+
+  // set GL viewport (not called by wxGLCanvas::OnSize on all platforms...)
+  int width, height;
+  GetClientSize(&width, &height);
+
+  if (_graphics_window.valid())
+    {
+      // update the window dimensions, in case the window has been resized.
+      _graphics_window->getEventQueue()->windowResize(0, 0, width, height);
+      _graphics_window->resized(0,0,width,height);
+    }
+}
+
+void osgCanvas::OnChar(wxKeyEvent &event)
+{
+#if wxUSE_UNICODE
+  int key = event.GetUnicodeKey();
+#else
+  int key = event.GetKeyCode();
+#endif
+
+  if (_graphics_window.valid())
+    _graphics_window->getEventQueue()->keyPress(key);
+
+  // If this key event is not processed here, we should call
+  // event.Skip() to allow processing to continue.
+}
+
+void osgCanvas::OnKeyUp(wxKeyEvent &event)
+{
+#if wxUSE_UNICODE
+  int key = event.GetUnicodeKey();
+#else
+  int key = event.GetKeyCode();
+#endif
+
+  if (_graphics_window.valid())
+    _graphics_window->getEventQueue()->keyRelease(key);
+
+  // If this key event is not processed here, we should call
+  // event.Skip() to allow processing to continue.
+}
+
+void osgCanvas::OnMouseEnter(wxMouseEvent &event)
+{
+  // Set focus to ourselves, so keyboard events get directed to us
+  SetFocus();
+}
+
+void osgCanvas::OnMouseDown(wxMouseEvent &event)
+{
+  if (_graphics_window.valid())
+    {
+      _graphics_window->getEventQueue()->mouseButtonPress(event.GetX(), event.GetY(),
+                                                          event.GetButton());
+    }
+}
+
+void osgCanvas::OnMouseUp(wxMouseEvent &event)
+{
+  if (_graphics_window.valid())
+    {
+      _graphics_window->getEventQueue()->mouseButtonRelease(event.GetX(), event.GetY(),
+                                                            event.GetButton());
+    }
+}
+
+void osgCanvas::OnMouseMotion(wxMouseEvent &event)
+{
+  if (_graphics_window.valid())
+    _graphics_window->getEventQueue()->mouseMotion(event.GetX(), event.GetY());
+}
+
+void osgCanvas::OnMouseWheel(wxMouseEvent &event)
+{
+  int delta = event.GetWheelRotation() / event.GetWheelDelta() * event.GetLinesPerAction();
+
+  if (_graphics_window.valid()) {
+    _graphics_window->getEventQueue()->mouseScroll(
+                                                   delta>0 ?
+                                                   osgGA::GUIEventAdapter::SCROLL_UP :
+                                                   osgGA::GUIEventAdapter::SCROLL_DOWN);
+  }
+}
+
+//-------------------------------------------------------------------------------------
+
--- osgcontrol.hpp
+++ osgcontrol.hpp
@@ -1,20 +1,64 @@
 #ifndef OSGCONTROL_HPP
 #define OSGCONTROL_HPP
 
-#include <wx/control.h>
+#include <wx/glcanvas.h>
 #include <osgViewer/Viewer>
 
-//@TODO: przerobić na wxGLCanvas https://github.com/openscenegraph/osg/tree/master/examples/osgviewerWX
-class osgControl : public wxControl
+class osgCanvas : public wxGLCanvas
 {
 public:
-  osgControl();
-  ~osgControl();
+  osgCanvas(wxWindow *parent, wxWindowID id = wxID_ANY,
+            const wxPoint& pos = wxDefaultPosition,
+            const wxSize& size = wxDefaultSize, long style = 0,
+            const wxString& name = wxT("TestGLCanvas"),
+            int *attributes = 0);
 
-  osgViewer::Viewer* Viewer;
+  virtual ~osgCanvas();
 
-private:
-  void OnPaint(wxPaintEvent&);
+  void OnPaint(wxPaintEvent& event);
+  void OnSize(wxSizeEvent& event);
+  //void OnEraseBackground(wxEraseEvent& event); /* Do nothing, to avoid flashing on MSW */
+
+  void OnChar(wxKeyEvent &event);
+  void OnKeyUp(wxKeyEvent &event);
+
+  void OnMouseEnter(wxMouseEvent &event);
+  void OnMouseDown(wxMouseEvent &event);
+  void OnMouseUp(wxMouseEvent &event);
+  void OnMouseMotion(wxMouseEvent &event);
+  void OnMouseWheel(wxMouseEvent &event);
+
+  osg::ref_ptr<osgViewer::GraphicsWindow> _graphics_window;
+
+};
+
+//-----------------------------------------------------------
+
+class wxGraphicsWindow : public osgViewer::GraphicsWindow
+{
+public:
+  osgCanvas* Canvas;
+
+  wxGraphicsWindow(osgCanvas* canvas);
+  ~wxGraphicsWindow();
+
+  void init();
+
+  //
+  // GraphicsWindow interface
+  //
+  void grabFocus();
+  void grabFocusIfPointerInWindow();
+  void useCursor(bool cursorOn);
+
+  bool makeCurrentImplementation();
+  void swapBuffersImplementation();
+
+  virtual bool valid() const { return true; }
+  virtual bool realizeImplementation() { return true; }
+  virtual bool isRealizedImplementation() const { return Canvas->IsShownOnScreen(); }
+  virtual void closeImplementation() {}
+  virtual bool releaseContextImplementation() { return true; }
 };
 
 #endif
